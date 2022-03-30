2020449dffcab6bd8f5fb0f90d9f66e614d1ecde
SmokeGuap <87711568+SmokeGuap@users.noreply.github.com>
2022-03-30 13:49:34 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -6,13 +6,16 @@ void Init_Container(Container* Head, Container* Tail) {
     Head->Prev = Tail->Prev = NULL;
     Head->Len = Tail->Len = 0;
 }
+
 void In_Container(Container* Head, Container* Tail, ifstream& ifst) {
     Container* Temp;
     int Len = 0; 
+
     while (!ifst.eof()) {
         Temp = new Container(); 
         Temp->Next = NULL;
-        Temp->Prev = NULL;        
+        Temp->Prev = NULL;
+        
         if (!Len) {
             if ((Head->Cont = In_Car(ifst))) {
                 Tail = Head;
@@ -27,7 +30,9 @@ void In_Container(Container* Head, Container* Tail, ifstream& ifst) {
                 Len++;
             }
         }
-    }    
+    }
+
+ 
     for (int i = 0; i < Len; i++) {
         Head->Len = Len;
         if (Head->Next) {
@@ -39,10 +44,15 @@ void In_Container(Container* Head, Container* Tail, ifstream& ifst) {
 void Out_Container(Container* Head, ofstream& ofst) {
     ofst << "Container contains " << Head->Len
         << " elements." << endl << endl;
-    Container* Temp = Head; 
+
+    Container* Temp = Head;
+
     for (int i = 0; i < Head->Len; i++) {
         ofst << i << ": ";
         Out_Car(Temp->Cont, ofst);
+
+        ofst << "Load to capacity ratio is = " << Load_to_capacity_ratio(Temp->Cont) << endl << endl;
+
         if (Temp->Next) {
             Temp = Temp->Next;
         }
@@ -54,27 +64,65 @@ void Clear_Container(Container* Head, Container* Tail) {
     
     for (int i = 0; i < Head->Len; i++) {
         free(Temp->Cont);
-        Temp->Len = 0;        
+        Temp->Len = 0;
+        
         if (Temp->Next) {
             Temp = Temp->Next;
             free(Temp->Prev);
         }
+
     }
+
     Head->Len = 0;
+}
+
+bool Compare(Container* First, Container* Second) {
+    return Load_to_capacity_ratio(First->Cont) > Load_to_capacity_ratio(Second->Cont);
+}
+
+void Sort(Container* Head) {
+    if (Head->Len > 1) {
+        Container* First = Head;
+        Container* Second = Head->Next;
+
+        Container* Temp = new Container;
+
+        for (int i = 0; i < Head->Len - 1; i++) {
+            for (int j = 0; j < Head->Len - i - 1; j++) {
+                if (Compare(First, Second)) {
+                    Temp->Cont = First->Cont;
+                    First->Cont = Second->Cont;
+                    Second->Cont = Temp->Cont;
+                }
+
+                Second = Second->Next;
+            }
+
+            First = First->Next;
+            Second = First->Next;
+        }
+    }
 }
 
 Car* In_Car(ifstream& ifst) {
     Car* C; 
     int K;
+
     ifst >> K; 
-    if (K == 1) {       
+
+    if (K == 1) {
+       
         C = (Car*)In_Truck(ifst); 
-        C->K = TRUCK;
+
+        C->K = TRUCK; 
+
         return C;
     }
     else if (K == 2) {
         C = (Car*)In_Bus(ifst); 
+
         C->K = BUS; 
+
         return C;
     }
     else {
@@ -84,7 +132,7 @@ Car* In_Car(ifstream& ifst) {
 
 void Out_Car(Car* C, ofstream& ofst) {
     if (C->K == TRUCK) {
-        Out_Truck((Truck*)C, ofst); 
+        Out_Truck((Truck*)C, ofst);
     }
     else if (C->K == BUS) {
         Out_Bus((Bus*)C, ofst); 
@@ -94,8 +142,23 @@ void Out_Car(Car* C, ofstream& ofst) {
     }
 }
 
+double Load_to_capacity_ratio(Car* C) {
+    if (C->K == TRUCK)
+    {
+        return Load_to_capacity_ratio_Truck((Truck*)C); 
+    }
+    else if (C->K == BUS)
+    {//Подсчет отношения груза к мощности автобуса
+    }
+    else
+    {
+        return -1;
+    }
+}
+
 Truck* In_Truck(ifstream& ifst) {
     Truck* T = new Truck();
+
     ifst >> T->Motor_power;
     ifst >> T->Load_cap;
 
@@ -104,17 +167,28 @@ Truck* In_Truck(ifstream& ifst) {
 
 
 void Out_Truck(Truck* T, ofstream& ofst) {
-    ofst << "It's a Truck with motor power = " << T->Motor_power<< endl;
-    ofst << "Its load capacity is " << T->Load_cap << endl << endl;
+    ofst << "Truck with motor power = " << T->Motor_power<< endl;
+    ofst << "Load capacity is " << T->Load_cap << endl;
+}
+
+double Load_to_capacity_ratio_Truck(Truck* T) {
+    return (double)T->Load_cap / (double)T->Motor_power;
 }
 
 Bus* In_Bus(ifstream& ifst) {
     Bus* B = new Bus();
+
     ifst >> B->Motor_power;
     ifst >> B->Passenger_cap;
+
     return B;
 }
+
 void Out_Bus(Bus* B, ofstream& ofst) {
-    ofst << "It's a Bus with motor power = " << B->Motor_power << endl;
-    ofst << "Its passenger capacity is " << B->Passenger_cap << endl << endl;
+    ofst << "Bus with motor power = " << B->Motor_power << endl;
+    ofst << "Passenger capacity is " << B->Passenger_cap << endl;
+}
+
+double Load_to_capacity_ratio_Bus(Bus* B) {
+    return (double)(75 * B->Passenger_cap) / (double)B->Motor_power;
 }
